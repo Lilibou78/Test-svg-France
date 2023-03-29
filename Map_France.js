46680c0999da3fefe80a849de474c85d5bfee459
Lilibou78 <correa.leyson@gmail.com>
2023-03-29 07:16:09 +0200

    mise a jour SVG interface

--- Map_France.js
+++ Map_France.js
@@ -9,17 +9,48 @@ var links = map.querySelectorAll('.list__region a');
 	}
 }*/
 
+const check = (carte) =>{
+	console.log(carte);
+	if (carte.includes('region') || carte.includes('liste')){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+const activeArea = (ids, vrai) => {
+console.log(vrai);
+		map.querySelectorAll('.is-active').forEach(item => item.classList.remove("is-active"));
+
+	if (ids !== null && vrai !== false){
+		document.querySelector('#region_' + ids).classList.add("is-active")
+		document.querySelector('#liste-' + ids).classList.add("is-active");
+	}
+}
 
 paths.forEach( path => {
 	path.addEventListener('mouseenter', e => {
-		var id = e.target.id;
-		console.log(id);
-		map.querySelectorAll('.is-active').forEach(item => item.classList.remove("is-active"));
-		e.target.classList.add("is-active");
-		console.log(id.replace('region_','liste-'));
-		var list = id.replace('region_','liste-');
-		document.getElementById(list).classList.add("is-active");
-		console.log(list);
+	let test = check(e.target.id);
+		if (test === true) {
+			var id = e.target.id.replace('region_','');
+			activeArea(id);
+		}else{
+			activeArea(id,test);
+		}
 	});
 });
 
+links.forEach( link => {
+	link.addEventListener('mouseenter', e => {
+	let test = check(e.target.id);
+		if (test === true) {
+			var id = e.target.id.replace('liste-','');
+			activeArea(id);			
+		}else{
+			activeArea(id,test);
+		}
+	});
+});
+
+
+map.addEventListener('mouseleave', e => activeArea());
